affe899bae7edf06fdf418c5b6799cdacaff0a1d
d.gugunava <d.gugunava@infosec.ru>
2021-07-22 10:34:50 +0300

    Initial commit

---
+++ src/main/java/ru/danil/reflection/converter/ReflectionConverterApplication.java
@@ -0,0 +1,12 @@
+package ru.danil.reflection.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReflectionConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReflectionConverterApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/danil/reflection/converter/ReflectionConverterApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.danil.reflection.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReflectionConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
